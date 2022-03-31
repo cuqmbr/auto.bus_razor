6a3bb0ac6596e36781e54a99fd023d61353ec7a1
cuqmbr <dr.juniorf@gmail.com>
2022-03-31 21:16:48 +0300

    chore: code cleanup

--- TicketOffice/Pages/Index.cshtml.cs
+++ TicketOffice/Pages/Index.cshtml.cs
@@ -7,31 +7,11 @@ namespace TicketOffice.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly TicketOfficeContext _context;
-    
     public IndexModel(TicketOfficeContext context)
     {
-        _context = context;
     }
 
-    public IActionResult OnGet()
+    public void OnGet()
     {
-        return Page();
-    }
-
-    [BindProperty]
-    public User User { get; set; }
-    
-    public async Task<IActionResult> OnPostAsync()
-    {
-        if (!ModelState.IsValid)
-        {
-            return Page();
-        }
-
-        _context.User.Add(User);
-        await _context.SaveChangesAsync();
-        
-        return RedirectToPage("./Routes");
     }
 }
