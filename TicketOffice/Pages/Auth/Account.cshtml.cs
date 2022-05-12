3bf86ff66e70b16d23ff9399800eb35fdc349414
cuqmbr <dr.juniorf@gmail.com>
2022-05-12 18:30:53 +0300

    feat: add account page where user can see bought tickets

--- TicketOffice/Pages/Auth/Account.cshtml.cs
+++ TicketOffice/Pages/Auth/Account.cshtml.cs
@@ -8,5 +8,49 @@ namespace TicketOffice.Pages.Account;
 
 public class IndexModel : PageModel
 {
+    public List<Ticket> Tickets { get; set; }
+    [BindProperty(SupportsGet = true)] public int ReturnTicketId { get; set; }
     
+    private readonly TicketOfficeContext _context;
+    private readonly ILogger<IndexModel> _logger;
+    
+    public IndexModel(TicketOfficeContext context, ILogger<IndexModel> logger)
+    {
+        _context = context;
+        _logger = logger;
+    }
+
+    public IActionResult OnGet()
+    {
+        if (!ValidateSession())
+            return RedirectToPage("/Auth/Login");
+
+        Tickets = _context.Ticket
+                .Where(t => t.UserId == HttpContext.Session.GetInt32("UserId"))
+                .Include(t => t.Route)
+                .Include(t => t.Route.Cities)
+                .ToList();
+
+        return Page();
+    }
+
+    public IActionResult OnGetReturnTicket()
+    {
+        OnGet();
+        
+        _logger.Log(LogLevel.Information, $"\n\n\n\n {ReturnTicketId} \n\n\n\n");
+        
+        Ticket returnTicket = _context.Ticket.Find(ReturnTicketId);
+
+        if (returnTicket is not null)
+        {
+            _context.Remove(returnTicket);
+            _context.SaveChanges();
+            return RedirectToPage("./Account");
+        }
+        
+        return NotFound();
+    }
+    
+    private bool ValidateSession() => HttpContext.Session.GetInt32("UserId") is not null;
 }
